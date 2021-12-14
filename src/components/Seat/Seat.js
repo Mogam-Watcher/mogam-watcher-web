fa8e95e1f097602af5f8124f43909042d037427c
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-14 12:00:37 +0100

    Create Component Seat #4
    - Move 'Seat.css' into 'Seat' folder
    - update files 'Seat.js' and 'Seat.css'

--- src/components/Seat/Seat.js
+++ src/components/Seat/Seat.js
@@ -1,7 +1,16 @@
 import './Seat.css'
 
-const Seat = () => {
+const Seat = ({isOccupied, until}) => {
+  const className=isOccupied?'seat occupied':'seat vacant';
+  const time=isOccupied?until:'';
 
+  return (
+    <div className={className}>
+      <span>
+          {time}
+        </span>
+    </div>
+  );
 }
 
 export default Seat;
