d0d8e5c9a2613149306e5a1b8a274b97b73ccad8
Sungdae Kim <hotag9@gmail.com>
2021-12-14 16:52:02 +0100

    Create Component TextButton #8
    - change prop name (onClick -> clickEvent) in TextButton.js
    - add cursor style in TextButton.css

--- src/components/TextButton/TextButton.js
+++ src/components/TextButton/TextButton.js
@@ -1,14 +1,14 @@
 import './TextButton.css'
 
-const TextButton = ({isSubmit, title, onClick}) => {
+const TextButton = ({isSubmit, title, clickEvent}) => {
   const styleVariant = `button textButton`;
   const buttonType = isSubmit ? "submit" : "button";
 
   return (
-    <button type={buttonType} className={styleVariant} onClick={onClick}>
+    <button type={buttonType} className={styleVariant} onClick={clickEvent}>
       {title}
     </button>
-  )
+  );
 }
 
 export default TextButton;
