53675d635925d4daa4684418ef619e64a726c6de
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-14 15:28:23 +0100

    Create Component Seat #4
    - Add empty spaces in elvis operators.

--- src/components/Seat/Seat.js
+++ src/components/Seat/Seat.js
@@ -1,8 +1,8 @@
 import './Seat.css'
 
 const Seat = ({isOccupied, endTime}) => {
-  const styleVariant=isOccupied?'seat seat-occupied':'seat seat-vacant';
-  const time=isOccupied?endTime:'';
+  const styleVariant = isOccupied ? 'seat seat-occupied' : 'seat seat-vacant';
+  const time = isOccupied ? endTime : '';
 
   return (
     <div className={styleVariant}>
