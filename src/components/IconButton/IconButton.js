7cbe7c2b13ff48dcf1922eeab2e79323906d4a2c
Hyun Kim <kmasterh@gmail.com>
2021-12-14 19:43:11 +0100

    Add prop(alt) [IconButton.js]

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./IconButton.css";
 
-const IconButton = ({ imageURL, onClickToDo, iconClassName }) => {
+const IconButton = ({ imageURL, altContent, onClickToDo, iconClassName }) => {
   return (
     <button type="iconButton" onClick={onClickToDo} className="icon-button">
-      <img alt="icon" src={imageURL} className={iconClassName} />
+      <img alt={altContent} src={imageURL} className={iconClassName} />
     </button>
   )
 }
