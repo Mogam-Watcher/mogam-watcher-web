48dedbac0d6a7ae77977a5d0ca8b02a93ed56588
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-18 05:27:41 +0100

    Create Component SelectorBlock #7
    - Update selected value with Hook.

--- src/components/SelectorBlock/SelectorBlock.js
+++ src/components/SelectorBlock/SelectorBlock.js
@@ -1,6 +1,13 @@
+import React, { useState } from 'react';
 import './SelectorBlock.css'
 
 const SelectorBlock = ({identification, title, dataSet}) => {
+  const [selectedValue, setValue] = useState('default');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   const optionData = (data) => {
       return (
         <option key={data.key} value={data.contents}>
@@ -14,8 +21,8 @@ const SelectorBlock = ({identification, title, dataSet}) => {
       <label htmlFor={identification} className='selector-block selector-block-label'>
         {title} :
       </label>
-      <select id={identification} className='selector-block selector-block-select'>
-        <option value='default' disabled hidden>선택하세요</option>
+      <select id={identification} value={selectedValue} onChange={handleChange} className='selector-block selector-block-select'>
+        <option key='0' value='default' disabled hidden>선택하세요</option>
         {dataSet.map(optionData)}
       </select>
     </>
