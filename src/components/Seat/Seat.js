ac8604d342f5deb3255359c3e880e6f2bf56aa98
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-18 02:16:46 +0100

    Create Component Seat #4
    - Change the Prop name. (endTime -> seatInfo)

--- src/components/Seat/Seat.js
+++ src/components/Seat/Seat.js
@@ -1,11 +1,11 @@
 import './Seat.css'
 
-const Seat = ({seatNumber, isOccupied, endTime, clickEvent}) => {
+const Seat = ({seatNumber, isOccupied, seatInfo, clickEvent}) => {
   const seatStyleVariant = isOccupied ? 'seat seat-occupied' : 'seat seat-vacant';
   const seatNumberStyleVariant = isOccupied ? 'seat-number seat-number-occupied' : 'seat-number seat-number-vacant';
   const voidFunction = () => {};
   const event = isOccupied ? voidFunction : clickEvent;
-  const info = isOccupied ? endTime : '';
+  const info = isOccupied ? seatInfo : '';
 
   return (
     <button 
