e6d0e3ac55aed9ef7eda1ab1c02674346c85b881
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-18 04:43:14 +0100

    Create Component SelectorBlock #7
    - update files 'SelectorBlock.js' and 'SelectorBlock.css'

--- src/components/SelectorBlock/SelectorBlock.js
+++ src/components/SelectorBlock/SelectorBlock.js
@@ -1,7 +1,25 @@
 import './SelectorBlock.css'
 
-const SelectorBlock = () => {
+const SelectorBlock = ({identification, title, dataSet}) => {
+  const optionData = (data) => {
+      return (
+        <option key={data.key} value={data.contents}>
+          {data.contents}
+        </option>
+      )
+  };
 
+  return (
+    <>
+      <label htmlFor={identification} className='selector-block selector-block-label'>
+        {title} :
+      </label>
+      <select id={identification} className='selector-block selector-block-select'>
+        <option value='default' disabled hidden>선택하세요</option>
+        {dataSet.map(optionData)}
+      </select>
+    </>
+  );
 }
 
 export default SelectorBlock;
