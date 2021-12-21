1613c5c5c7219b4a69f5ecefc6a3b00b3622e333
SunnyJinny <hyejin.kang@rwth-aachen.de>
2021-12-21 23:43:56 +0100

    Modify grid style

--- src/components/SeatMap/SeatMap.js
+++ src/components/SeatMap/SeatMap.js
@@ -1,29 +1,28 @@
 import React from "react";
-import "./SeatMap.css"
-/*TODO change TestSeat -> Seat component*/
-import TestSeat from "../TestSeat/TestSeat";
+import "./SeatMap.css";
+import Seat from "../Seat/Seat"
 
 const SeatMap = () => {
   /*TODO remove: 가상 데이터 */
   const testSeatArray = [ 
-    { seatNumber: 'seat0', isOccupied: true, endTime: '18:00' },
-    { seatNumber: 'seat1', isOccupied: true, endTime: '14:00' },
-    { seatNumber: 'seat2', isOccupied: true, endTime: '14:00' },
-    { seatNumber: 'seat3', isOccupied: false},
-    { seatNumber: 'seat4', isOccupied: false},
-    { seatNumber: 'seat5', isOccupied: false}, 
-    { seatNumber: 'seat-6', isOccupied: false},
-    { seatNumber: 'seat-7', isOccupied: true, endTime: '21:00'}, 
-    { seatNumber: 'seat-8', isOccupied: false},
-    { seatNumber: 'seat-9', isOccupied: true, endTime: '17:00'}, 
-    { seatNumber: 'seat-10', isOccupied: false},
-    { seatNumber: 'seat-11', isOccupied: false},
+    { seatNumber: '0', isOccupied: true, endTime: '18:00' },
+    { seatNumber: '1', isOccupied: true, endTime: '14:00' },
+    { seatNumber: '2', isOccupied: true, endTime: '14:00' },
+    { seatNumber: '3', isOccupied: false},
+    { seatNumber: '4', isOccupied: false},
+    { seatNumber: '5', isOccupied: false}, 
+    { seatNumber: '6', isOccupied: false},
+    { seatNumber: '7', isOccupied: true, endTime: '21:00'}, 
+    { seatNumber: '8', isOccupied: false},
+    { seatNumber: '9', isOccupied: true, endTime: '17:00'}, 
+    { seatNumber: '10', isOccupied: false},
+    { seatNumber: '11', isOccupied: false},
   ]
 
   return (
     <div className="seatMap-container">
       {testSeatArray.map(seat => 
-        <TestSeat seatNumber={seat.seatNumber} isOccupied={seat.isOccupied} endTime={seat.endTime} />
+        <Seat seatId={"seat"+seat.seatNumber} seatNumber={seat.seatNumber} isOccupied={seat.isOccupied} seatInfo={seat.endTime} />
       )}
     </div>
     )
