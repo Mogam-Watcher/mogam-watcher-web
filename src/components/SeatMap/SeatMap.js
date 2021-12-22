0a3f9fddcaf6c2f82a451a9c24d96842b2bfa7e3
SunnyJinny <53663147+SunnyJinny@users.noreply.github.com>
2021-12-22 16:58:32 +0100

    Create component seat map #5 (#18)
    * Create SeatMap Component branch
    
    - Add grid css in the case of mobile phone and tablet&PC

---
+++ src/components/SeatMap/SeatMap.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./SeatMap.css";
+import Seat from "../Seat/Seat"
+
+const SeatMap = () => {
+  /*TODO remove: 가상 데이터 */
+  const testSeatArray = [ 
+    { seatNumber: '0', isOccupied: true, endTime: '18:00' },
+    { seatNumber: '1', isOccupied: true, endTime: '14:00' },
+    { seatNumber: '2', isOccupied: true, endTime: '14:00' },
+    { seatNumber: '3', isOccupied: false},
+    { seatNumber: '4', isOccupied: false},
+    { seatNumber: '5', isOccupied: false}, 
+    { seatNumber: '6', isOccupied: false},
+    { seatNumber: '7', isOccupied: true, endTime: '21:00'}, 
+    { seatNumber: '8', isOccupied: false},
+    { seatNumber: '9', isOccupied: true, endTime: '17:00'}, 
+    { seatNumber: '10', isOccupied: false},
+    { seatNumber: '11', isOccupied: false},
+  ]
+
+  return (
+    <div className="seatMap-container">
+      {testSeatArray.map(seat => 
+        <Seat seatId={"seat"+seat.seatNumber} seatNumber={seat.seatNumber} isOccupied={seat.isOccupied} seatInfo={seat.endTime} />
+      )}
+    </div>
+    )
+}
+
+export default SeatMap;
