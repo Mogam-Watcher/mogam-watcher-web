504ac8467a740e87a4231a854c7444dd71229c93
Sungdae Kim <hotag9@gmail.com>
2021-12-23 17:43:48 +0100

    Create Component Form #24
    - Update for mobile and box-sizing: border-box;
    - Update formBlock : elvis -> arrow function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import './Form.css'
 
 const Form = ({identification, title, type, dataSet, formHint, formValue}) => {
-  const defaultValue = (type==='select' && typeof formValue==='undefined') ? 'default' : formValue;
-  const [selectedValue, setValue] = useState(defaultValue);
+  const defaultValue = (type==='select') ? 'default' : formValue;
+  const [value, setValue] = useState(defaultValue);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -11,34 +11,49 @@ const Form = ({identification, title, type, dataSet, formHint, formValue}) => {
 
   const optionData = (data) => {
       return (
-        <option key={data.key} value={data.contents}>
+        <option className='form-block-seletor' key={data.key} value={data.contents}>
           {data.contents}
         </option>
       )
   };
 
-  const formBlock = (type==='select') ? (
-      <select id={identification} value={selectedValue} onChange={handleChange} className='form form-block'>
-        <option key='0' value='default' disabled hidden>{formHint}</option>
-        {dataSet.map(optionData)}
-      </select>
-      ) : (
-      <input 
-        className="form form-block"
-        id={identification} 
-        type={type}
-        name={title} 
-        placeholder={formHint}
-        value={selectedValue}
-        onChange={handleChange}
-      />);
+  const formBlock = (type) => {
+    switch (type) {
+      case 'select':
+        return (
+          <select 
+            id={identification}
+            value={value}
+            onChange={handleChange}
+            className='form form-block'
+          >
+            <option key='0' value='default' disabled hidden>
+              {formHint}
+            </option>
+            {dataSet.map(optionData)}
+          </select>
+        );
+      case 'text':
+        return (
+          <input 
+            className='form form-block'
+            id={identification} 
+            type={type}
+            name={title} 
+            placeholder={formHint}
+            value={value}
+            onChange={handleChange}
+          />
+        );
+    }
+  }  
 
   return (
     <div className='form'>
-      <label htmlFor={identification} className='form form-label'>
+      <label htmlFor={identification} className='form-label'>
         {title} :
       </label>
-      {formBlock}
+      {formBlock(type)}
     </div>
   );
 }
