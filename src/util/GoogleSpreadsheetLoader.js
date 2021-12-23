dbd2ab908ecbcec0637fffcad283ba3a1d712203
FelixDev0518 <yoseobshim9695@gmail.com>
2021-12-23 22:31:37 +0100

    save

--- src/util/GoogleSpreadsheetLoader.js
+++ src/util/GoogleSpreadsheetLoader.js
@@ -5,43 +5,113 @@ const auth = new google.auth.GoogleAuth({
   scopes: "https://www.googleapis.com/auth/spreadsheets"
 });
 
+const tableInfoScheme = [
+  {
+  tableNumber: 0,
+  tableRange:"C6:D6",
+  dataToUpdate: ["홍길동","10:00"]
+  },
+  {
+    tableNumber: 1,
+    tableRange:"C8:D8",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 2,
+    tableRange:"C10:D10",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 3,
+    tableRange:"C12:D12",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 4,
+    tableRange:"C14:D14",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 5,
+    tableRange:"C16:D16",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 6,
+    tableRange:"C18:D18",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 7,
+    tableRange:"C20:D20",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 8,
+    tableRange:"C22:D22",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 9,
+    tableRange:"C24:D24",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 10,
+    tableRange:"C26:D26",
+    dataToUpdate: ["",""]
+  },
+  {
+    tableNumber: 11,
+    tableRange:"C28:D28",
+    dataToUpdate: ["",""]
+  },
+]
+
 const spreadsheetId = "1WZA7BAh1WESgTuWmFDI9lSR92Ub87gur2ct-a1YqB3c"
 
-async function getSpreadsheetData(tableRange) {
+async function getSpreadsheetData(number) {
   
   const client = await auth.getClient();
   
   const googleSheets = google.sheets({version: "v4", auth: client});
   
-  const dataFromSheet = await googleSheets.spreadsheets.values.get({
-    auth,
-    spreadsheetId,
-    range : `시트1!${tableRange}`
-  });
+  if(number == tableInfoScheme[number].tableNumber){
+    const dataFromSheet = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range : `시트1!${tableInfoScheme.tableRange}`
+    });
+
+    return dataFromSheet.data;
+  }
   
-  return dataFromSheet.data;
+  
+  
 }
 
-async function updateSpreadsheetData(data){
+async function updateSpreadsheetData(number, data) {
 
   const client = await auth.getClient();
   
   const googleSheets = google.sheets({version: "v4", auth: client});
+  if(number == tableInfoScheme[number].tableNumber){
+    await googleSheets.spreadsheets.values.update({
+      auth,
+      spreadsheetId,
+      range : `시트1!${tableInfoScheme.tableRange}`,
+      valueInputOption:'USER_ENTERED',
+      resource:{
+        values:[
+          data.dataToUpdate
+        ]
+      }
+    })
+  }
   
-  await googleSheets.spreadsheets.values.update({
-    auth,
-    spreadsheetId,
-    range : `시트1!${data.tableRange}`,
-    valueInputOption:'USER_ENTERED',
-    resource:{
-      values:[
-        data.dataToUpdate
-      ]
-    }
-  })
 }
 
-async function deleteSpreadsheetData(data){
+async function deleteSpreadsheetData(data) {
   
   const client = await auth.getClient();
   
@@ -54,15 +124,4 @@ async function deleteSpreadsheetData(data){
   })
 }
 
-// const testData = {
-//   tableRange:"C6:D6",
-//   dataToUpdate: ["홍길동","10:00"]
-// }
-
-// // deleteSpreadsheetData(testData);
-// updateSpreadsheetData(testData);
-const data = getSpreadsheetData("C6:D6");
-data.then((e)=>{
-  console.log(e);
-})
 export default {getSpreadsheetData, updateSpreadsheetData, deleteSpreadsheetData};
