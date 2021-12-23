4840b4e0c74c05bd564681a080a2622fa730b063
FelixDev0518 <yoseobshim9695@gmail.com>
2021-12-23 14:38:35 +0100

    save

--- src/util/GoogleSpreadsheetLoader.js
+++ src/util/GoogleSpreadsheetLoader.js
@@ -1,11 +1,11 @@
 import {google} from 'googleapis';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "../../util/credentials.json",
+  keyFile: "./credentials.json",
   scopes: "https://www.googleapis.com/auth/spreadsheets"
 });
 
-const spreadsheetId = "<SPREADSHEET-ID>"
+const spreadsheetId = "1WZA7BAh1WESgTuWmFDI9lSR92Ub87gur2ct-a1YqB3c"
 
 async function getSpreadsheetData(tableRange) {
   
@@ -16,14 +16,14 @@ async function getSpreadsheetData(tableRange) {
   const dataFromSheet = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
-    range : `<SHEET NAME>!${tableRange}`
+    range : `시트1!${tableRange}`
   });
   
   return dataFromSheet.data;
 }
 
 async function updateSpreadsheetData(data){
-  
+
   const client = await auth.getClient();
   
   const googleSheets = google.sheets({version: "v4", auth: client});
@@ -31,7 +31,7 @@ async function updateSpreadsheetData(data){
   await googleSheets.spreadsheets.values.update({
     auth,
     spreadsheetId,
-    range : `<SHEET NAME>!${data.tableRange}`,
+    range : `시트1!${data.tableRange}`,
     valueInputOption:'USER_ENTERED',
     resource:{
       values:[
@@ -50,8 +50,19 @@ async function deleteSpreadsheetData(data){
   await googleSheets.spreadsheets.values.clear({
     auth,
     spreadsheetId,
-    range : `<SHEET NAME>!${data.tableRange}`,
+    range : `시트1!${data.tableRange}`,
   })
 }
 
+// const testData = {
+//   tableRange:"C6:D6",
+//   dataToUpdate: ["홍길동","10:00"]
+// }
+
+// // deleteSpreadsheetData(testData);
+// updateSpreadsheetData(testData);
+const data = getSpreadsheetData("C6:D6");
+data.then((e)=>{
+  console.log(e);
+})
 export default {getSpreadsheetData, updateSpreadsheetData, deleteSpreadsheetData};
