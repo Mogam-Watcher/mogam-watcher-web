f7e23bb4d3daceaba6978258b3103a1e12f1f9d5
FelixDev0518 <yoseobshim9695@gmail.com>
2021-12-24 20:05:52 +0100

    Module CRUD Function #14
    - Deleted MogamWatcherViewModel and MogamWatcherModel

--- src/models/MogamWatcherViewModel.js
+++
@@ -1,5 +0,0 @@
-import MogamWatcherModel from "./domain/MogamWatcherModel";
-
-class MogamWatcherViewModel{
- /** TODO ViewModel will be implemented */
-}
--- src/models/domain/MogamWatcherModel.js
+++
@@ -1,7 +0,0 @@
-import loader from '../../util/GoogleSpreadsheetLoader.js'
-
-class MogamWatcherModel {
-  /**MogamWatcherModel will be implemented */
-}
-
-export default MogamWatcherModel;
