2116420c862be66e7d7f8a5b34391271fa2da2f8
FelixDev0518 <yoseobshim9695@gmail.com>
2021-12-24 20:00:49 +0100

    Module CRUD Function #14
    - Deleted the 'type:module' of package.json becuase of error
    - Changed file extension of env and GoogleSpreadsheetLoader because of error
    - Add tableInfoScheme in GoogleSpreadsheetLoader.mjs

--- src/util/GoogleSpreadsheetLoader.mjs
+++ src/util/GoogleSpreadsheetLoader.mjs
@@ -1,74 +1,63 @@
-import {google} from 'googleapis';
+import { google } from 'googleapis';
+import './env.mjs';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./credentials.json",
+  keyFile: process.env.CREDENTIAL_PATH,
   scopes: "https://www.googleapis.com/auth/spreadsheets"
 });
 
 const tableInfoScheme = [
   {
   tableNumber: 0,
-  tableRange:"C6:D6",
-  dataToUpdate: ["홍길동","10:00"]
+  tableRange:"C6:D6"
   },
   {
     tableNumber: 1,
-    tableRange:"C8:D8",
-    dataToUpdate: ["",""]
+    tableRange:"C8:D8"
   },
   {
     tableNumber: 2,
-    tableRange:"C10:D10",
-    dataToUpdate: ["",""]
+    tableRange:"C10:D10"
   },
   {
     tableNumber: 3,
-    tableRange:"C12:D12",
-    dataToUpdate: ["",""]
+    tableRange:"C12:D12"
   },
   {
     tableNumber: 4,
-    tableRange:"C14:D14",
-    dataToUpdate: ["",""]
+    tableRange:"C14:D14"
   },
   {
     tableNumber: 5,
-    tableRange:"C16:D16",
-    dataToUpdate: ["",""]
+    tableRange:"C16:D16"
   },
   {
     tableNumber: 6,
-    tableRange:"C18:D18",
-    dataToUpdate: ["",""]
+    tableRange:"C18:D18"
   },
   {
     tableNumber: 7,
-    tableRange:"C20:D20",
-    dataToUpdate: ["",""]
+    tableRange:"C20:D20"
   },
   {
     tableNumber: 8,
-    tableRange:"C22:D22",
-    dataToUpdate: ["",""]
+    tableRange:"C22:D22"
   },
   {
     tableNumber: 9,
-    tableRange:"C24:D24",
-    dataToUpdate: ["",""]
+    tableRange:"C24:D24"
   },
   {
     tableNumber: 10,
-    tableRange:"C26:D26",
-    dataToUpdate: ["",""]
+    tableRange:"C26:D26"
   },
   {
     tableNumber: 11,
-    tableRange:"C28:D28",
-    dataToUpdate: ["",""]
+    tableRange:"C28:D28"
   },
 ]
 
-const spreadsheetId = "1WZA7BAh1WESgTuWmFDI9lSR92Ub87gur2ct-a1YqB3c"
+const spreadsheetId = process.env.SPREADSHEET_ID;
 
 async function getSpreadsheetData(number) {
   
@@ -80,14 +69,11 @@ async function getSpreadsheetData(number) {
     const dataFromSheet = await googleSheets.spreadsheets.values.get({
       auth,
       spreadsheetId,
-      range : `시트1!${tableInfoScheme.tableRange}`
+      range : `시트1!${tableInfoScheme[number].tableRange}`
     });
-
+    
     return dataFromSheet.data;
   }
-  
-  
-  
 }
 
 async function updateSpreadsheetData(number, data) {
@@ -95,15 +81,16 @@ async function updateSpreadsheetData(number, data) {
   const client = await auth.getClient();
   
   const googleSheets = google.sheets({version: "v4", auth: client});
+
   if(number == tableInfoScheme[number].tableNumber){
-    await googleSheets.spreadsheets.values.update({
+    googleSheets.spreadsheets.values.update({
       auth,
       spreadsheetId,
-      range : `시트1!${tableInfoScheme.tableRange}`,
-      valueInputOption:'USER_ENTERED',
-      resource:{
-        values:[
-          data.dataToUpdate
+      range: `시트1!${tableInfoScheme[number].tableRange}`,
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: [
+          data
         ]
       }
     })
@@ -111,17 +98,18 @@ async function updateSpreadsheetData(number, data) {
   
 }
 
-async function deleteSpreadsheetData(data) {
+async function deleteSpreadsheetData(number) {
   
   const client = await auth.getClient();
   
   const googleSheets = google.sheets({version: "v4", auth: client});
-  
-  await googleSheets.spreadsheets.values.clear({
-    auth,
-    spreadsheetId,
-    range : `시트1!${data.tableRange}`,
-  })
+  if(number == tableInfoScheme[number].tableNumber){
+    await googleSheets.spreadsheets.values.clear({
+      auth,
+      spreadsheetId,
+      range : `시트1!${tableInfoScheme[number].tableRange}`,
+    })
+  }
 }
 
 export default {getSpreadsheetData, updateSpreadsheetData, deleteSpreadsheetData};
