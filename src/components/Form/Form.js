8190f08b0ebb9c321476f714d90f79afe95c5f2f
Sungdae Kim <hotag9@gmail.com>
2021-12-24 08:09:56 +0100

    Create Component Form #24
    - Update Form.js changed arrow-img in form-block

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -25,7 +25,7 @@ const Form = ({identification, title, type, dataSet, formHint, formValue}) => {
             id={identification}
             value={value}
             onChange={handleChange}
-            className='form form-block'
+            className='form-block form-block-arrow selectNative js-selectNative'
           >
             <option key='0' value='default' disabled hidden>
               {formHint}
@@ -36,7 +36,7 @@ const Form = ({identification, title, type, dataSet, formHint, formValue}) => {
       case 'text':
         return (
           <input 
-            className='form form-block'
+            className='form-block'
             id={identification} 
             type={type}
             name={title} 
