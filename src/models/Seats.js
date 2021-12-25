98983c390d91e25bae676150c82b3054191bd523
Sungdae Kim <hotag9@gmail.com>
2021-12-25 12:45:11 +0100

    Module Json parser #20
    - Fix error when there is no value in the sheet

--- src/models/Seats.js
+++ src/models/Seats.js
@@ -1,24 +1,21 @@
-/* 
-
 const testDataSet = {
   range: "'시트1'!C6:D29",
   majorDimension: 'ROWS',
   values: [
     [ '홍길동', '10:00' ], [],
-    [ '구간남', '8:00' ],  [],
     [ '공간남', '9:00' ],  [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ], [],
-    [ '홍길동', '11:00' ]
+    [ '공간남', '9:00' ],  [],
+    [ '1홍길동', '11:00' ], [],
+    [ '2홍길동', '11:00' ], [],
+    [ '3홍길동', '11:00' ], [],
+    [ '4홍길동', '11:00' ], [],
+    [ '5홍길동', '11:00' ], [],
+    [ '6홍길동', '11:00' ], [],
+    [ '7홍길동', '11:00' ], [],
+    [], [],
+    [ '8홍길동', '11:00' ]
   ]
 };
-*/
 
 class Seats {
   constructor(total) {
@@ -27,16 +24,20 @@ class Seats {
     this.seats = new Array();
 
     for (let i = 0; i < total; i ++){
-      let data = JSON.stringify(testDataSet.values[i]);
-      if(i % 2 == 1){
-        data = JSON.stringify(testDataSet.values[i+1]);
-      }
+      let data = JSON.stringify(testDataSet.values[(i * 2)]);
       let isUsed = isUsedSeat(data);
-      data = sheetPaser(data);
       let tableRange = getTableRange(i);
-      this.seats[i] = { number: i, userName: data[0], endTime: data[1], isUsed: isUsed, tableRange: tableRange};
+    
+      if(isUsed){
+        data = sheetPaser(data);
+        this.seats[i] = { number: i, userName: data[0], endTime: data[1], isUsed: isUsed, tableRange: tableRange};
+      } else {
+        this.seats[i] = { number: i, userName: '', endTime: '', isUsed: isUsed, tableRange: tableRange};
+      }
+      console.log(this);
     }
     countFreeSeat(this);
+    
   }
 }
 
@@ -49,7 +50,7 @@ function countFreeSeat(Seats){
 }
 
 function isUsedSeat(seatData){
-  if(seatData.indexOf(':00') == -1){
+  if(seatData.length === 2){
     return false;
   }
   return true;
