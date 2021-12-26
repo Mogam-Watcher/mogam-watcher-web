011486690284f7c36a8de34543a8ae3180a0e7d6
Sungdae Kim <hotag9@gmail.com>
2021-12-26 08:22:24 +0100

    Module Json parser #20
    - Update Seats.js variable names

--- src/models/Seats.js
+++ src/models/Seats.js
@@ -18,45 +18,31 @@ const testDataSet = {
 };
 
 class Seats {
-  constructor(total) {
-    this.total = total;
-    this.countFreeSeat = total;
+  constructor(totalSeat) {
+    this.totalSeat = totalSeat;
+    this.countVacantSeat = totalSeat;
     this.seats = new Array();
-
-    for (let i = 0; i < total; i ++){
-      let data = JSON.stringify(testDataSet.values[(i * 2)]);
-      let isUsed = isUsedSeat(data);
-      let tableRange = getTableRange(i);
-    
-      if(isUsed){
-        data = sheetPaser(data);
-        this.seats[i] = { number: i, userName: data[0], endTime: data[1], isUsed: isUsed, tableRange: tableRange};
-      } else {
-        this.seats[i] = { number: i, userName: '', endTime: '', isUsed: isUsed, tableRange: tableRange};
-      }
-      console.log(this);
-    }
-    countFreeSeat(this);
-    
+    seatsInit(totalSeat, this.seats);
+    countVacantSeat(this);
   }
 }
 
-function countFreeSeat(Seats){
+function countVacantSeat(Seats) {
   for(let seat of Seats.seats){
-    if(seat.isUsed){
-      Seats.countFreeSeat--;
+    if(seat.isOccupied){
+      Seats.countVacantSeat--;
     }
   }
 }
 
-function isUsedSeat(seatData){
+function isOccupiedSeat(seatData) {
   if(seatData.length === 2){
     return false;
   }
   return true;
 }
 
-function sheetPaser(seatData){
+function sheetPaser(seatData) {
   const data = seatData.split(',');
   data[0] = data[0].substring(2, 5);
   if(data[1].length == 7){
@@ -67,8 +53,22 @@ function sheetPaser(seatData){
   return data;
 }
 
-function getTableRange(seatNumber){
+function getTableRange(seatNumber) {
   const index = [6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28];
   return `!C${index[seatNumber]}:D${index[seatNumber]}`;
+}
+
+function seatsInit(totalSeat, seats) {
+  for (let i = 0; i < totalSeat; i ++){
+    let data = JSON.stringify(testDataSet.values[(i * 2)]);
+    let isOccupied = isOccupiedSeat(data);
+    let tableRange = getTableRange(i);
+    if(isOccupied){
+      data = sheetPaser(data);
+      seats[i] = { number: i, userName: data[0], endTime: data[1], isOccupied: isOccupied, tableRange: tableRange};
+    } else {
+      seats[i] = { number: i, userName: '', endTime: '', isOccupied: isOccupied, tableRange: tableRange};
+    }
+  }
 }
 export default Seats;
