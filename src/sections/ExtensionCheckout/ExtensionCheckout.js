50eda5195706d8218875d9526116cb5a5bfa8595
SunnyJinny <hyejin.kang@rwth-aachen.de>
2021-12-28 10:42:28 +0100

    Create Section Extension/Checkout.js and Extension/Checkout.css

---
+++ src/sections/ExtensionCheckout/ExtensionCheckout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Form from '../../components/Form/Form';
+import TextButton from '../../components/TextButton/TextButton';
+import './ExtensionCheckout.css';
+
+const ExtensionCheckout = () => {
+  const set = [{key:1, contents:"12:00"}, 
+               {key:2, contents:"13:00"}, 
+               {key:3, contents:"14:00"}, 
+               {key:4, contents:"15:00"}, 
+               {key:5, contents:"16:00"}];
+  return (
+    <div className="checkoutModal-container">
+      {/*TODO 좌석번호 데이터받아유 */} 
+      <span className="checkoutModal-item" id="checkoutModal-title">좌석번호 7</span>
+      <div className="checkoutModal-item"><Form identification='' title='이름' type='text' formHint='이름' /></div>
+      <div className="checkoutModal-item"><Form identification='' title='연장시간' type='select' formHint='희망연장시간' dataSet={set} /></div>
+      <div className="checkoutModal-item" id="extensionButton"><TextButton buttonType="submit" title="Extension" clickEvent="#" /></div>
+      <hr className="horizontalRules"/>
+      <div className="checkoutModal-item" id="checkoutButton"><TextButton buttonType="submit" title="Check Out" clickEvent="#" /></div>
+    </div>
+  );
+};
+
+export default ExtensionCheckout;
