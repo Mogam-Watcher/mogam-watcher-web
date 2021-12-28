f1c57c023e3a633fea5d9081d4ccfe23cd4b03f3
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-28 21:42:47 +0100

    Apply modification/new mode into component/section #28
    - Add the onClick-event for the Seat.
    - Delete clickEvent from the Props.

--- src/components/Seat/Seat.js
+++ src/components/Seat/Seat.js
@@ -1,21 +1,43 @@
 import React from "react";
-import './Seat.css'
+import './Seat.css';
+import useModal from '../../components/ModalPopup/useModal';
+import ModalPopup from '../../components/ModalPopup/ModalPopup';
+import RegistrationForm from '../../sections/RegistrationForm/RegistrationForm';
 
-const Seat = ({seatId, seatNumber, isOccupied, seatInfo, clickEvent}) => {
+const Seat = ({seatId, seatNumber, isOccupied, seatInfo}) => {
+  const {isShowing, toggle} = useModal();
   const seatStyleVariant = isOccupied ? 'seat seat-occupied' : 'seat seat-vacant';
   const seatNumberStyleVariant = isOccupied ? 'seat-number seat-number-occupied' : 'seat-number seat-number-vacant';
   const info = isOccupied ? seatInfo : '';
+  const popup = () => {
+    /*TODO popup for isOccupied===true*/
+    if(isOccupied) {
+      return (
+        <p>
+          "TODO"
+        </p>
+      );
+    } else {
+      return (
+        <RegistrationForm seatNumber={seatNumber}></RegistrationForm>
+      );
+    }
+  };
 
   return (
-    /*TODO clickEvent link*/
-    <button type='button' id={seatId} className={seatStyleVariant} onClick={clickEvent}>
-      <div className={seatNumberStyleVariant}>
-        {seatNumber}
-      </div>
-      <div className='seat-info'>
-        {info}
-      </div>
-    </button>
+    <>
+      <button type='button' id={seatId} className={seatStyleVariant} onClick={toggle}>
+        <div className={seatNumberStyleVariant}>
+          {seatNumber}
+        </div>
+        <div className='seat-info'>
+          {info}
+        </div>
+      </button>
+      <ModalPopup isShowing={isShowing} hide={toggle}>
+        {popup()}
+      </ModalPopup>
+    </>
   );
 }
 
