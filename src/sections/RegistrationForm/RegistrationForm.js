9fceab25f5839ce271f26478e3c8a8cb46760798
SunnyJinny <hyejin.kang@rwth-aachen.de>
2021-12-28 10:37:27 +0100

    Modify (flex)css

--- src/sections/RegistrationForm/RegistrationForm.js
+++ src/sections/RegistrationForm/RegistrationForm.js
@@ -1,26 +1,22 @@
 import React from "react";
-import useModal from '../../components/ModalPopup/useModal';
 import Form from '../../components/Form/Form';
 import TextButton from '../../components/TextButton/TextButton';
 import './RegistrationForm.css';
 
 const RegistrationForm = () => {
-  const {isShowing, toggle} = useModal();
   const set = [{key:1, contents:"12:00"}, 
                {key:2, contents:"13:00"}, 
                {key:3, contents:"14:00"}, 
                {key:4, contents:"15:00"}, 
                {key:5, contents:"16:00"}];
   return (
-   <div className="App">
-    <div className="modalContent-wrapper">
+    <div className="checkInModal-container">
       {/*TODO 좌석번호 데이터받아유 */}
-      <h1 className="formTitle-wrapper">좌석번호 7</h1>
-      <div className="formContent-wrapper"><Form identification='' title='이름' type='text' formHint='이름' /></div>
-      <div className="formContent-wrapper"><Form identification='' title='퇴실시간' type='select' formHint='예상퇴실시간' dataSet={set} /></div>
-      <div className="formButton-wrapper"><TextButton buttonType="submit" title="Check In" clickEvent="#" /></div>
+      <span className="checkInModal-item" id="checkInModal-title">좌석번호 7</span>
+      <div className="checkInModal-item"><Form identification='' title='이름' type='text' formHint='이름' /></div>
+      <div className="checkInModal-item"><Form identification='' title='퇴실시간' type='select' formHint='예상퇴실시간' dataSet={set} /></div>
+      <div className="checkInModal-item"><TextButton buttonType="submit" title="Check In" clickEvent="#" /></div>
     </div>
-  </div>
   );
 };
 
