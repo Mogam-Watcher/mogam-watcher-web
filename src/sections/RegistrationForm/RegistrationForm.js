fb60002be8f5feefe50529a6d04e2d3b48f1395b
Sungdae Kim <hotag9@gmail.com>
2021-12-28 21:48:53 +0100

    Apply data model into components #33
    - Add RegistationFrom.js function checkIn()
        checkIn() : check the data entered in the form
                    change the data in the object
    - Add RegistationFrom.js useState to get data from child component
    - Update Form.js 1 props is added to send input data

--- src/sections/RegistrationForm/RegistrationForm.js
+++ src/sections/RegistrationForm/RegistrationForm.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from '../../components/Form/Form';
 import BaseButton from '../../components/BaseButton/BaseButton';
 import './RegistrationForm.css';
 
 const RegistrationForm = ({seatNumber}) => {
+  const [userName, setUserName] = useState();
+  const [endTime, setEndTime] = useState();
+
   function getEndTimeSet() {
     const currentTime = new Date().getHours();
     const maxEndTime = 24;
@@ -12,6 +15,17 @@ const RegistrationForm = ({seatNumber}) => {
       endTimeSet[i] = {key: i + 1, contents: `${currentTime + i + 1}:00`};
     }
     return endTimeSet;
+  }
+  const checkIn = () => {
+    const confirmMessage = `${userName}님 ${seatNumber}번 자리 ${endTime}까지 신청하시겠습니까?`
+    if(typeof userName === 'undefined'){
+      alert('이름을 입력해주세요');
+    } else if(typeof endTime === 'undefined'){
+      alert('예상 퇴실시간을 입력해주세요');
+    } else if(confirm(confirmMessage)){
+      seatArray[seatNumber] = {userName: userName, endTime: endTime, isOccupied: true}
+      alert(`${userName}님 ${seatNumber}번 자리 ${endTime}까지 신청되셨습니다.`)
+    }
   }
   
   return (
@@ -24,8 +38,10 @@ const RegistrationForm = ({seatNumber}) => {
          identification='' 
          title='이름' 
          type='text' 
-         formHint='이름' 
+         formHint='이름'
+         formValue='' 
          isDisabled={false} 
+         setData={setUserName}
         />
       </div>
       <div className="checkInModal-item">
@@ -35,14 +51,15 @@ const RegistrationForm = ({seatNumber}) => {
          type='select' 
          formHint='예상퇴실시간' 
          dataSet={getEndTimeSet()} 
-         isDisabled={false} 
+         isDisabled={false}
+         setData={setEndTime} 
         />
       </div>
       <div className="checkInModal-item">
         <BaseButton 
          buttonType="submit" 
          title="Check In" 
-         clickEvent="#" 
+         clickEvent={checkIn} 
         />
       </div>
     </div>
