ffec412f6a3c2e8f8504625acc48d835f61ccb12
SeRin-Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-29 16:22:58 +0100

    Apply modification/new mode into component/section #28
    - Use the naming convention.

--- src/sections/RegistrationForm/RegistrationForm.js
+++ src/sections/RegistrationForm/RegistrationForm.js
@@ -11,11 +11,35 @@ const RegistrationForm = ({seatNumber}) => {
                {key:5, contents:"16:00"}];
   return (
     <div className="checkInModal-container">
-      {/*TODO 좌석번호 데이터받아유 */}
-      <span className="checkInModal-item" id="checkInModal-title">좌석번호 {seatNumber}</span>
-      <div className="checkInModal-item"><Form identification='' title='이름' type='text' formHint='이름' /></div>
-      <div className="checkInModal-item"><Form identification='' title='퇴실시간' type='select' formHint='예상퇴실시간' dataSet={set} /></div>
-      <div className="checkInModal-item"><BaseButton buttonType="submit" title="Check In" clickEvent="#" /></div>
+      <span className="checkInModal-item" id="checkInModal-title">
+        좌석번호 {seatNumber}
+      </span>
+      <div className="checkInModal-item">
+        <Form 
+         identification='' 
+         title='이름' 
+         type='text' 
+         formHint='이름' 
+         isDisabled={false} 
+        />
+      </div>
+      <div className="checkInModal-item">
+        <Form 
+         identification='' 
+         title='퇴실시간' 
+         type='select' 
+         formHint='예상퇴실시간' 
+         dataSet={set} 
+         isDisabled={false} 
+        />
+      </div>
+      <div className="checkInModal-item">
+        <BaseButton 
+         buttonType="submit" 
+         title="Check In" 
+         clickEvent="#" 
+        />
+      </div>
     </div>
   );
 };
