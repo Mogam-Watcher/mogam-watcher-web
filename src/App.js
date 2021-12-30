d6135e90b87fb6c1f78eeb25d06a083655c6a4d5
Sungdae Kim <hotag9@gmail.com>
2021-12-30 20:47:36 +0100

    Apply data model into components #33
    Add useContext in App.js to use Seats in all components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
+import Seats from './models/Seats';
 
+
+const SeatContext = createContext();
 const App = () => {
+  const seats = new Seats(12);
   return (
-    <div className="App">
-      <h1>Mogam watcher application</h1>
-    </div>
+    <SeatContext.Provider value={(seats)}>
+      <div className="App">
+        <h1>Mogam watcher application</h1>
+      </div>
+    </SeatContext.Provider>
   );
 }
-
+export {SeatContext};
 export default App;
