12abfae6aedfb44da13e0944a8f254049a01e956
Se Rin Yang <81328960+SeRin-Yang@users.noreply.github.com>
2021-12-30 15:00:54 +0100

    Apply modification/new mode into component/section #28 (#48)
    * Apply modification/new mode into component/section #28
    - Add seatNumber as a property (in RegistrationForm.js/ExtensionCheckout.js).
    - Add the onClick-event for the Seat.

--- src/sections/ExtensionCheckout/ExtensionCheckout.js
+++ src/sections/ExtensionCheckout/ExtensionCheckout.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Form from '../../components/Form/Form';
-import TextButton from '../../components/TextButton/TextButton';
+import BaseButton from '../../components/BaseButton/BaseButton';
 import './ExtensionCheckout.css';
 
-const ExtensionCheckout = () => {
+const ExtensionCheckout = ({seatNumber}) => {
   const set = [{key:1, contents:"12:00"}, 
                {key:2, contents:"13:00"}, 
                {key:3, contents:"14:00"}, 
@@ -11,13 +11,43 @@ const ExtensionCheckout = () => {
                {key:5, contents:"16:00"}];
   return (
     <div className="checkoutModal-container">
-      {/*TODO 좌석번호 데이터 받아주세요 */} 
-      <span className="checkoutModal-item" id="checkoutModal-title">좌석번호 7</span>
-      <div className="checkoutModal-item"><Form identification='' title='이름' type='text' formHint='이름' isDisabled={true}/></div>
-      <div className="checkoutModal-item"><Form identification='' title='연장시간' type='select' formHint='희망연장시간' dataSet={set} isDisabled={false}/></div>
-      <div className="checkoutModal-item" id="extensionButton"><TextButton buttonType="submit" title="Extension" clickEvent="#" /></div>
+      <span className="checkoutModal-item" id="checkoutModal-title">
+        좌석번호 {seatNumber}
+      </span>
+      <div className="checkoutModal-item">
+        <Form
+         identification=''
+         title='이름' 
+         type='text' 
+         formHint='이름' 
+         isDisabled={true}
+        />
+      </div>
+      <div className="checkoutModal-item">
+        <Form
+         identification='' 
+         title='연장시간' 
+         type='select' 
+         formHint='희망연장시간' 
+         dataSet={set} 
+         isDisabled={false}
+        />
+      </div>
+      <div className="checkoutModal-item" id="extensionButton">
+        <BaseButton
+         buttonType="submit" 
+         title="Extension" 
+         clickEvent="#" 
+        />
+      </div>
       <hr className="horizontalRules"/>
-      <div className="checkoutModal-item" id="checkoutButton"><TextButton buttonType="submit" title="Check Out" clickEvent="#" /></div>
+      <div className="checkoutModal-item" id="checkoutButton">
+        <BaseButton
+         buttonType="submit" 
+         title="Check Out" 
+         clickEvent="#" 
+        />
+      </div>
     </div>
   );
 };
--- src/sections/RegistrationForm/RegistrationForm.js
+++ src/sections/RegistrationForm/RegistrationForm.js
@@ -3,7 +3,7 @@ import Form from '../../components/Form/Form';
 import BaseButton from '../../components/BaseButton/BaseButton';
 import './RegistrationForm.css';
 
-const RegistrationForm = () => {
+const RegistrationForm = ({seatNumber}) => {
   const set = [{key:1, contents:"12:00"}, 
                {key:2, contents:"13:00"}, 
                {key:3, contents:"14:00"}, 
@@ -11,11 +11,35 @@ const RegistrationForm = () => {
                {key:5, contents:"16:00"}];
   return (
     <div className="checkInModal-container">
-      {/*TODO 좌석번호 데이터받아유 */}
-      <span className="checkInModal-item" id="checkInModal-title">좌석번호 7</span>
-      <div className="checkInModal-item"><Form identification='' title='이름' type='text' formHint='이름' /></div>
-      <div className="checkInModal-item"><Form identification='' title='퇴실시간' type='select' formHint='예상퇴실시간' dataSet={set} /></div>
-      <div className="checkInModal-item"><BaseButton buttonType="submit" title="Check In" clickEvent="#" /></div>
+      <span className="checkInModal-item" id="checkInModal-title">
+        좌석번호 {seatNumber}
+      </span>
+      <div className="checkInModal-item">
+        <Form 
+         identification='' 
+         title='이름' 
+         type='text' 
+         formHint='이름' 
+         isDisabled={false} 
+        />
+      </div>
+      <div className="checkInModal-item">
+        <Form 
+         identification='' 
+         title='퇴실시간' 
+         type='select' 
+         formHint='예상퇴실시간' 
+         dataSet={set} 
+         isDisabled={false} 
+        />
+      </div>
+      <div className="checkInModal-item">
+        <BaseButton 
+         buttonType="submit" 
+         title="Check In" 
+         clickEvent="#" 
+        />
+      </div>
     </div>
   );
 };
