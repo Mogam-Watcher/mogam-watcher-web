e1ba76fe9391d94ba10bf16ec4d041155de4dddc
Yoseob Shim <59021311+FelixDev0518@users.noreply.github.com>
2022-01-01 23:22:09 +0100

    Module crud function #14 (#27)
    * Module CRUD Function #14
    - Add GoogleSpreadsheetLoader.js with CRUD functions
    - Add 'type': 'module' in package.json to use 'import' in MogamWatcherModel.js
    - Modify MogamWatcherModel.js and GoogleSpreadsheetLoader.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,11 @@
 const path = require('path');
 
+const dotenv = require('dotenv')
+dotenv.config();
+
+const webpack = require('webpack');
+
+
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   module: {
@@ -24,7 +30,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx']
   },
   output: {
     path: path.resolve(__dirname, './public'),
@@ -34,5 +40,26 @@ module.exports = {
     static: path.resolve(__dirname, './public'),
     hot: 'only',
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+      'process.env.FIREBASE_MESSURMENT_ID': JSON.stringify(process.env.FIREBASE_MESSURMENT_ID),
+    }),
+    new webpack.EnvironmentPlugin([
+      'FIREBASE_API_KEY',
+      'FIREBASE_AUTH_DOMAIN',
+      'FIREBASE_DATABASE_URL',
+      'FIREBASE_PROJECT_ID',
+      'FIREBASE_STORAGE_BUCKET',
+      'FIREBASE_MESSAGING_SENDER_ID',
+      'FIREBASE_APP_ID',
+      'FIREBASE_MESSURMENT_ID']),
+  ]
 };
 
